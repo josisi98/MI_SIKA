cb68db5e808436e4101982aff9362d23385c7272
josisi98 <122314411+josisi98@users.noreply.github.com>
2023-09-29 07:20:13 +0000

    code bac

--- app.py
+++ app.py
@@ -20,6 +20,21 @@ class Utilisateur(db.Model):
 
     def __repr__(self):
         return f'Utilisateur({self.nom}, {self.email})'
+    
+    # Modèle de données pour chaque étape du formulaire
+class Etape(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    statut_professionnel = db.Column(db.String(50))
+    revenus_annuels_2023 = db.Column(db.Float)
+    progression_annuelle = db.Column(db.Integer)
+    age = db.Column(db.Integer)
+    mode_versements = db.Column(db.String(10))
+    versements_annee = db.Column(db.Float)
+    versements_proportionnels = db.Column(db.Float)
+    age_retraite = db.Column(db.Integer)
+    versements_fixes = db.Column(db.Float)
+    progression_annuelle_revenus = db.Column(db.Integer)
+
 
 # Modèle Message
 class Message(db.Model):
@@ -86,6 +101,67 @@ def apropos():
 @app.route('/simulateur')
 def simulateur():
     return render_template('Simulateur.html', title='Simulateur')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Récupérer les données du formulaire et enregistrer dans la base de données
+        etape = Etape(
+            statut_professionnel=request.form.get('STAT_PRO'),
+            revenus_annuels_2023=float(request.form.get('REV_0', 0)),
+            progression_annuelle=int(request.form.get('TX_AUG_SAL', 0)),
+            age=int(request.form.get('AGE_0', 0)),
+            mode_versements=request.form.get('MOD_VERS', ''),
+            versements_annee=float(request.form.get('VERS_0', 0)),
+            versements_proportionnels=float(request.form.get('PERC_SAL', 0)),
+            age_retraite=int(request.form.get('AGE_END', 0)),
+            versements_fixes=float(request.form.get('VERS_ANN', 0)),
+            progression_annuelle_revenus=int(request.form.get('TX_AUG_SAL', 0))
+        )
+        db.session.add(etape)
+        db.session.commit()
+        
+        # Rediriger vers l'étape suivante
+        return redirect(url_for('etape_suivante'))
+
+    return render_template('index.html')
+@app.route('/simulation', methods=['POST'])
+def simulation():
+    # Récupérer les données du formulaire
+    age_actuel = int(request.form.get('AGE_0', 0))
+    age_retraite = int(request.form.get('AGE_END', 0))
+    mode_versements = request.form.get('MOD_VERS')
+
+    # Initialiser les variables de simulation
+    annees_jusqu_retraite = age_retraite - age_actuel
+    simulation_results = []
+
+    if mode_versements == 'FIX':
+        # Simulation pour des versements fixes
+        montant_annuel = float(request.form.get('VERS_ANN', 0))
+
+        for annee in range(annees_jusqu_retraite):
+            simulation_results.append({
+                'annee': age_actuel + annee,
+                'montant_verse': montant_annuel
+            })
+    elif mode_versements == 'PROP':
+        # Simulation pour des versements proportionnels au salaire
+        pourcentage_salaire = float(request.form.get('PERC_SAL', 0))
+        revenus_annuels_2023 = float(request.form.get('REV_0', 0))
+        taux_augmentation_salaire = float(request.form.get('TX_AUG_SAL', 0)) / 100
+
+        revenus_annuels = revenus_annuels_2023
+        for annee in range(annees_jusqu_retraite):
+            montant_verse = (pourcentage_salaire / 100) * revenus_annuels
+            simulation_results.append({
+                'annee': age_actuel + annee,
+                'montant_verse': montant_verse
+            })
+            revenus_annuels *= (1 + taux_augmentation_salaire)
+
+    return render_template('simulation_resultats.html', simulation_results=simulation_results)
+
 
 @app.route('/contact', methods=['POST'])
 def contact():
